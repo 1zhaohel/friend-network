63a64da0b1f098d605065275e0d308310d256545
Helen <129004013+1zhaohel@users.noreply.github.com>
2024-03-31 12:02:13 -0400

    add: description and requirements file

---
+++ compute.py
@@ -0,0 +1,18 @@
+"""CSC111 Winter 2024 Project 2: Compute on Data (Part 2)
+
+Instructions (READ THIS FIRST!)
+===============================
+
+This Python module contains classes responsible for computing on the data.
+
+Copyright and Usage Information
+===============================
+
+This file is provided solely for the personal and private use of students
+taking CSC111 at the University of Toronto St. George campus. All forms of
+distribution of this code, whether as given or with any changes, are
+expressly prohibited. For more information on copyright for CSC111 materials,
+please consult our Course Syllabus.
+
+This file is Copyright (c) 2024 CSC111 Teaching Team
+"""
---
+++ data.py
@@ -0,0 +1,19 @@
+"""CSC111 Winter 2024 Project 2: Reading Data (Part 1)
+
+Instructions (READ THIS FIRST!)
+===============================
+
+This Python module contains classes responsible for reading data from our dataset
+and creating a graph.
+
+Copyright and Usage Information
+===============================
+
+This file is provided solely for the personal and private use of students
+taking CSC111 at the University of Toronto St. George campus. All forms of
+distribution of this code, whether as given or with any changes, are
+expressly prohibited. For more information on copyright for CSC111 materials,
+please consult our Course Syllabus.
+
+This file is Copyright (c) 2024 CSC111 Teaching Team
+"""
