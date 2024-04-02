7e5e65f5528ca3b1f4dbab830b76c389bce82213
alice5399 <alice5399@naver.com>
2024-04-02 02:39:46 -0400

    fix

--- visualize.py
+++ visualize.py
@@ -29,10 +29,10 @@ USER_COLOUR = 'rgb(105, 89, 205)'
 
 
 def visualize_graph(graph_tuple: tuple[data.Graph, data.WeightedGraph],
+                    start: str, end: str,
                     layout: str = 'spring_layout',
                     max_vertices: int = 5000,
-                    output_file: str = '', weighted: bool = False,
-                    path: list = None) -> None:
+                    output_file: str = '', weighted: bool = False) -> None:
     """Use plotly and networkx to visualize the given graph.
 
     Optional arguments:
@@ -48,52 +48,57 @@ def visualize_graph(graph_tuple: tuple[data.Graph, data.WeightedGraph],
 
     graph_nx = graph.conv_networkx(max_vertices)
     pos = getattr(nx, layout)(graph_nx)
-    # added
     nx.set_node_attributes(graph_nx, pos, 'pos')
 
     x_values = [pos[k][0] for k in graph_nx.nodes]
     y_values = [pos[k][1] for k in graph_nx.nodes]
     labels = list(graph_nx.nodes)
-    kinds = [graph_nx.nodes[k].get('kind', 'unknown') for k in graph_nx.nodes]
-    # kinds = [graph_nx.nodes[k]['kind'] for k in graph_nx.nodes]  # error
-
-    # TODO we can use this for if it is in the path or not
-    colours = [BOOK_COLOUR if kind == 'book' else USER_COLOUR for kind in kinds]
 
     x_edges = []
     y_edges = []
+    path = graph.get_friend_path(start, end)  # TODO check if returns list of edges (tuples of items)
+    x_highlight_edges = []
+    y_highlight_edges = []
+
     for edge in graph_nx.edges:
-        x_edges += [pos[edge[0]][0], pos[edge[1]][0], None]
-        y_edges += [pos[edge[0]][1], pos[edge[1]][1], None]
+        if edge in path:
+            x_highlight_edges += [pos[edge[0]][0], pos[edge[1]][0], None]
+            y_highlight_edges += [pos[edge[0]][1], pos[edge[1]][1], None]
+        else:
+            x_edges += [pos[edge[0]][0], pos[edge[1]][0], None]
+            y_edges += [pos[edge[0]][1], pos[edge[1]][1], None]
 
-    trace3 = Scatter(x=x_edges,
-                     y=y_edges,
-                     mode='lines',
-                     name='edges',
-                     line=dict(color='rgb(255,185,185)', width=1),
-                     hoverinfo='none',
-                     )
+    trace_highlight = Scatter(x=x_highlight_edges,
+                              y=y_highlight_edges,
+                              mode='lines',
+                              name='edges',
+                              line=dict(color='rgb(255,0,0)', width=1),
+                              hoverinfo='none',
+                              )
 
-    trace4 = Scatter(x=x_values,
-                     y=y_values,
-                     mode='markers',
-                     name='nodes',
-                     marker=dict(symbol='circle-dot',
-                                 size=5,
-                                 color='rgb(0,0,128)',
-                                 line=dict(color=VERTEX_BORDER_COLOUR, width=0.5)
-                                 ),
-                     text=labels,
-                     hovertemplate='%{text}',
-                     hoverlabel={'namelength': 0}
-                     )
+    trace_edges = Scatter(x=x_edges,
+                          y=y_edges,
+                          mode='lines',
+                          name='edges',
+                          line=dict(color='rgb(255,185,185)', width=1),
+                          hoverinfo='none',
+                          )
 
-    data1 = [trace3, trace4]
+    trace_nodes = Scatter(x=x_values,
+                          y=y_values,
+                          mode='markers',
+                          name='nodes',
+                          marker=dict(symbol='circle-dot',
+                                      size=5,
+                                      color='rgb(0,0,128)',
+                                      line=dict(color=VERTEX_BORDER_COLOUR, width=0.5)
+                                      ),
+                          text=labels,
+                          hovertemplate='%{text}',
+                          hoverlabel={'namelength': 0}
+                          )
 
-    # added
-    if path:
-        path_trace = highlight_path(graph_nx, path)
-        data1.append(path_trace)
+    data1 = [trace_highlight, trace_edges, trace_nodes]
 
     fig = Figure(data=data1)
     fig.update_layout({'showlegend': False})
@@ -104,24 +109,3 @@ def visualize_graph(graph_tuple: tuple[data.Graph, data.WeightedGraph],
         fig.show()
     else:
         fig.write_image(output_file)
-
-
-def highlight_path(graph_nx,
-                   path: list[tuple]):
-    """
-    Highlight the path between two plots.
-    """
-    x_edges = []
-    y_edges = []
-    for edge in path:
-        x_edges += [graph_nx.nodes[edge[0]]['pos'][0], graph_nx.nodes[edge[1]]['pos'][0], None]
-        y_edges += [graph_nx.nodes[edge[0]]['pos'][1], graph_nx.nodes[edge[1]]['pos'][1], None]
-
-    return Scatter(
-        x=x_edges,
-        y=y_edges,
-        mode='lines',
-        name='path',
-        line=dict(color='rgb(255,0,0)', width=3),
-        hoverinfo='none',
-    )
